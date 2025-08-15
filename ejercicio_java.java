ad0610b40c87772dd7de8c1b81e99b37d572404c
unknown <evers.uribeq@autonoma.edu.co>
2025-08-14 20:09:52 -0500

    primer commit

---
+++ ejercicio_java.java
@@ -0,0 +1,13 @@
+package ejercicios_primera_clase;
+public class ejercicio_java {
+    public static void main(String[] args) {
+        String [] productos;
+        String product_name;
+        int product_price;
+
+        
+
+    }
+
+ 
+}
